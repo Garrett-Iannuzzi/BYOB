67a87ae6c6ea4983eb25f5c92162b3ddd7686ff4
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-30 18:01:10 -0700

    Add get request for a metro by ID

--- server.js
+++ server.js
@@ -11,19 +11,36 @@ app.locals.title = 'BYOB';
 app.get('/api/v1/metros', async (request, response) => {
   try {
     const metros = await database('metros').select();
-    response.status(200).json(metros)
+    return response.status(200).json(metros)
   } catch(error) {
     console.error(error)
-    response.status(500).json("Internal Server Error")
+    return response.status(500).json("Internal Server Error")
   }
 });
 
 app.get('/api/v1/titles', async (request, response) => {
   try {
     const titles = await database('titles').select();
-    response.status(200).json(titles)
+    return response.status(200).json(titles)
   } catch(error) {
-    response.status(500).json("Internal Server Error")
+    return response.status(500).json("Internal Server Error")
+  }
+});
+
+app.get('/api/v1/metros/:id', async (request, response) => {
+  const { id } = request.params
+
+  if(!parseInt(id)) {
+    return response.status(422).json({ error: 'Incorrect ID:' + id })
+  }
+  try {
+    const metro  = await database('metros').where('id', id).select();
+    if(!metro.length) {
+      return response.status(404).json({ error: 'Could not find metro with ID:' + id })
+    }
+    return response.status(200).json({metro: metro[0]})
+  } catch(error) {
+    return response.status(500).json("Internal Server Error")
   }
 });
 
